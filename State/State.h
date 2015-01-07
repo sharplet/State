3f6cb3d725c600df7c63f910bb436350b7a07e2f
Adam Sharp <adsharp@me.com>
2015-01-07 18:03:04 +1100

    Initial Commit

---
+++ State/State.h
@@ -0,0 +1,19 @@
+//
+//  State.h
+//  State
+//
+//  Created by Adam Sharp on 7/01/2015.
+//  Copyright (c) 2015 Adam Sharp. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for State.
+FOUNDATION_EXPORT double StateVersionNumber;
+
+//! Project version string for State.
+FOUNDATION_EXPORT const unsigned char StateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <State/PublicHeader.h>
+
+
