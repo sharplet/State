7dc3737c93eb8fdf25aa2a422a406d39f7bb4cfe
Adam Sharp <adsharp@me.com>
2015-01-08 15:25:46 +1100

    Add tests for run(), exec() and eval()

--- StateTests/StateTests.swift
+++ StateTests/StateTests.swift
@@ -2,31 +2,19 @@
 //  Copyright (c) 2015 Adam Sharp. All rights reserved.
 //
 
-import Cocoa
+import State
 import XCTest
 
 class StateTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testRun() {
+        XCTAssert(((), [1]) == push(1).run([]))
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+
+    func testExec() {
+        XCTAssert([] == pop().exec([1]))
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+
+    func testEval() {
+        XCTAssert(1 == pop().eval([1]))
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
