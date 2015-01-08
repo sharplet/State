c5957ad5c311ee4ec23d723ac7e6f9ca99f9ff76
Adam Sharp <adsharp@me.com>
2015-01-08 15:54:02 +1100

    Add tests for get() and put()

--- StateTests/StateTests.swift
+++ StateTests/StateTests.swift
@@ -42,4 +42,27 @@ class StateTests: XCTestCase {
 
         XCTAssert([3] == add.exec([2,1]))
     }
+
+
+    // MARK: State operations
+
+    func testGetReadsTheCurrentState() {
+        XCTAssert([1] == get().eval([1]))
+    }
+
+    func testPutReplacesTheCurrentState() {
+        XCTAssert([3,2,1] == put([3,2,1]).exec([]))
+        XCTAssert([3,2,1] == put([3,2,1]).exec([4,5,6]))
+        XCTAssert([3,2,1] == put([3,2,1]).exec(Array(1...100)))
+    }
+
+    func testGetAndPutAllowReadingStateAtDifferentPointsInTime() {
+        let wrapString = get().flatMap { str in
+                         put("foo").then(get()).flatMap { foo in
+                         put("bar").then(get()).flatMap { bar in
+                             put("\(foo), \(str), \(bar)")
+                         }}}
+
+        XCTAssert("foo, hello, bar" == wrapString.exec("hello"))
+    }
 }
